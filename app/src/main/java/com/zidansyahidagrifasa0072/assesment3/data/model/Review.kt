714fe3821f0cf1dece48815749be28871f2dd5af
zidan <zid4nsyhid@gmail.com>
2026-06-22 20:35:13 +0700

    git commit -m "feat: add Review.kt"

---
+++ app/src/main/java/com/zidansyahidagrifasa0072/assesment3/data/model/Review.kt
@@ -0,0 +1,14 @@
+package com.zidansyahidagrifasa0072.assesment3.data.model
+
+data class Review(
+    val id: String = "",
+    val userId: String = "",
+    val userName: String = "",
+    val userEmail: String = "",
+    val userProfileUrl: String = "",
+    val placeName: String = "",
+    val description: String = "",
+    val rating: Float = 0f,
+    val imageUrl: String = "",
+    val timestamp: Long = System.currentTimeMillis()
+)
